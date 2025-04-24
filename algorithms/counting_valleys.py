94a058d66218ccdce8d9fc4145bcad59d1f4be5f
Shourai <10200748+Shourai@users.noreply.github.com>
2025-04-24 09:14:45 +0200

    Add counting valleys

--- algorithms/counting_valleys.py
+++ algorithms/counting_valleys.py
@@ -17,15 +17,29 @@ import sys
 
 
 def countingValleys(steps, path):
+    valley = False
+    amount_of_valleys = 0
     level = 0
+    valley_list = []
     for step in path:
         if step == "U":
             level += 1
         elif step == "D":
             level -= 1
-    print(level)
-    # Write your code here
+        if level < 0:
+            valley = True
+        elif level >= 0:
+            valley = False
+        valley_list.append(valley)
+    merged = []
+    for i, val in enumerate(valley_list):
+        if i == 0 or val != valley_list[i - 1]:
+            merged.append(val)
 
+    for i in merged:
+        if i == True:
+            amount_of_valleys += 1
+    return amount_of_valleys
 
 if __name__ == "__main__":
     steps = int(input().strip())
